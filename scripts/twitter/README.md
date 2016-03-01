8130b7edec73bd819f9a98d20c4ec6b5feb8c03b
Lionel Capdecomme <capdecomme.l@mipih.fr>
2016-03-01 16:20:24 +0100

    Script twitter

---
+++ scripts/twitter/README.md
@@ -0,0 +1,18 @@
+# Scripts twitter
+
+
+## piTweet.py 
+
+Ce script lit ce qu'il reçoit en entrée (pipe) et le tweet. Pour la configuration twitter, un document clair et détaillé : 
+
+1. http://www.makeuseof.com/tag/how-to-build-a-raspberry-pi-twitter-bot/
+
+2. 
+
+
+### utilisation 
+
+Par exemple : 
+
+*date | piTweet.py*
+
---
+++ scripts/twitter/piTweet.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+from twython import Twython
+CONSUMER_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+CONSUMER_SECRET = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+ACCESS_KEY = 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz'
+ACCESS_SECRET = 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'
+
+api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
+
+#api.update_status(status=sys.argv[1])
+message=""
+for line in sys.stdin:
+    message = message + line
+
+api.update_status(status=message)
